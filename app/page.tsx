72d44b89bb0a30b5940ccc40bf92d7edc0f52f98
David Sambilay <sambilay.david@gmail.com>
2024-02-03 17:06:18 -0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,272 @@
+'use client'
+
+import { useState } from "react";
+
 import Image from "next/image";
 
-export default function Home() {
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+// import { SiGmail } from "react-icons/si";
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from "@mui/material";
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+
+const drawerWidth = 240;
+const navItems = [{primary: 'Home', link: '#home'}, {primary: 'About', link: '#about'}, {primary: 'Projects', link: '#projects'}, {primary: 'Contact', link: '#contact'}];
+
+
+export default function Home(props: Props) {
+
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2, display: 'flex', justifyContent: 'center' }}>
+        David Sambilay
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.primary} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <Link href={item.link} color='inherit' underline="none">
+                <ListItemText primary={item.primary} />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  const githubRepoButton = (
+    <button className="flex items-center gap-2 bg-black hover:bg-gray-700 self-center rounded-lg p-2 mb-3 text-white">
+      <FaGithub size={20} />
+      Github
+    </button>
+  )
+
+
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+    <div>
+      <Box sx={{ display: 'flex' }}>
+        {/* <CssBaseline /> */}
+        <AppBar component="nav" color="primary">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              David Sambilay
+            </Typography>
+            
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              {navItems.map((item) => (
+                <Button href={item.link} key={item.primary} sx={{ color: '#fff' }} className="hover:bg-gray-800">
+                  {item.primary}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <nav>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            {drawer}
+          </Drawer>
+        </nav>
+      </Box>
+
+
+      <div className="bg-[url('./images/cs-background-pic.jpg')] bg-cover opacity-90 h-screen flex flex-col items-center justify-center gap-8 text-[#e1f3ff] text-center" id="home">
+        <h1 className="text-[3rem]">David Sambilay</h1>
+        <h2 className="text-xl">Aspiring Software Engineer</h2>
+      </div>
+
+      <div className="flex flex-col mx-3 my-6" id="about">
+        <h1 className="text-[3rem] text-center">About</h1>
+        <div className="flex flex-col md:flex-row gap-8 justify-around">
+          <img className="rounded-full w-[25rem] h-[25rem] object-cover max-md:mx-auto my-4" src="./images/about.jpg"/>
+          <div className="flex flex-col gap-5 justify-center text-left">
+            <h1 className="text-[2rem]">
+              Hi! I'm David.
+            </h1>
+            <h1>
+              I am an aspiring software engineer
+              proficient in web development. With
+              my passion to learn and hunger to excel, I am
+              driven to use my skills to bring about positive change
+              and pave a way for a better future.
+            </h1>
+            <h1>
+              When I'm not using my computer, I like to spend
+              my time playing basketball, playing guitar, weightlifting, and, most
+              of all, spending time with family and friends.
+            </h1>
+          </div>
+        </div>
+        <a href="./DavidSambilayResume.pdf" className="flex items-center gap-2 bg-black hover:bg-gray-700 self-center rounded-lg p-2 mb-3 mt-6 text-white no-underline">
+            Here's my resume!
           </a>
+      </div>
+
+      <div id="projects">
+        <h1 className="text-[3rem] text-center border-t-2 border-gray-150">Projects</h1>
+        <div className='flex flex-col lg:flex-row lg:flex-wrap justify-center gap-10 mx-10'>
+
+          <div className='flex flex-col justify-between shadow-xl rounded-lg lg:w-[30%] w-[100%] my-10'>
+            <div>
+              <Image
+                alt="mellow project" 
+                src="./images/mellow.png"
+                width={500}
+                height={500}
+                className="self-center"
+              />
+              <div className="flex flex-col justify-end m-3">
+                <h3 className='text-lg font-medium pt-8 pb-2'>Mellow</h3>
+                <p className='py-2'>
+                  A serverless full-stack to-do list application made with React and several AWS services. 
+                </p>
+                <h4 className='py-4 text-indigo-400'>Tech Stack:</h4>
+                <p className='text-gray-800 py-1'>React.js</p>
+                <p className='text-gray-800 py-1'>AWS Lambda, API Gateway</p>
+                <p className='text-gray-800 py-1'>AWS RDS Postgres</p>
+              </div>
+            </div>
+            <a href="https://github.com/KhaNguyen04/Mellow" className="flex items-center gap-2 bg-black hover:bg-gray-700 self-center rounded-lg p-2 mb-3 text-white no-underline">
+                <FaGithub size={20} />
+                Github
+            </a>
+          </div>
+          
+
+          <div className='flex flex-col justify-between shadow-xl rounded-lg lg:w-[30%] w-[100%]  my-10'>
+            <div>
+              <Image
+                alt="miscord project" 
+                src="./images/miscord.png"
+                width={500}
+                height={500}
+                className="self-center"
+              />
+              <div className="flex flex-col m-3">
+                <h3 className='text-lg font-medium pt-8 pb-2'>Miscord</h3>
+                <p className='py-2'>
+                  A simple chat application built in React inspired by Discord. Features include
+                  real-time messaging, single and group conversations.
+                </p>
+                <h4 className='py-4 text-indigo-400'>Tech Stack:</h4>
+                <p className='text-gray-800 py-1'>React.js</p>
+                <p className='text-gray-800 py-1'>Firebase</p>
+              </div>
+            </div>
+            <a href="https://github.com/iamgabelogan65/miscord" className="flex items-center gap-2 bg-black hover:bg-gray-700 self-center rounded-lg p-2 mb-3 text-white no-underline">
+                <FaGithub size={20} />
+                Github
+            </a>
+          </div>
+
+          <div className='flex flex-col justify-between shadow-xl rounded-lg lg:w-[30%] w-[100%] my-10'>
+            <div>
+              <Image
+                alt="design img" 
+                src="./images/auctionhouse.png"
+                width={500}
+                height={500}
+                className="self-center"                
+              />
+              <div className="flex flex-col m-3">
+                <h3 className='text-lg font-medium pt-8 pb-2'>Auction House</h3>
+                <p className='py-2'>
+                  An auction house web application that let users auction an 
+                  item and bid on an item
+                </p>
+                <h4 className='py-4 text-indigo-400'>Tech Stack:</h4>
+                <p className='text-gray-800 py-1'>React.js</p>
+                <p className='text-gray-800 py-1'>MySQL</p>
+                <p className='text-gray-800 py-1'>Spring Boot</p>
+              </div>
+            </div>
+
+            <a href="https://github.com/Shaion7/CS157A-Team5" className="flex items-center gap-2 bg-black hover:bg-gray-700 self-center rounded-lg p-2 mb-3 text-white no-underline">
+                <FaGithub size={20} />
+                Github
+            </a>
+          </div>
+
+
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="border-t-2 border-gray-150 pt-8" id="contact">
+        <h1 className="text-[2.5rem] text-center">Let's Connect!</h1>
+        <div className="flex md:flex-row flex-col justify-around m-8">
+          <div className="flex md:flex-row flex-col items-center gap-4">
+            <a href="https://github.com/iamgabelogan65"><FaGithub size={50} className="hover:text-gray-500 cursor-pointer" /></a>
+            <a href="https://www.linkedin.com/in/david-sambilay-4223081a3/"><FaLinkedin size={50} className="hover:text-gray-500 cursor-pointer" /></a>
+          </div>
+          <div className="flex flex-col items-center my-3">
+            <p>
+              EMAIL: davidsambilay3@gmail.com
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
